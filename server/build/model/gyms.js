0eb79f9c7d9367cd493edf2a39d18cb470365c96
yuzu-r <tkaneshiro@gmail.com>
2017-11-07 22:15:09 +0000

    split into client and server

---
+++ server/build/model/gyms.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GymsSchema = new Schema({
+	name: { type: String, min: 1, max: 999, required: true },
+	address: { type: String, max: 999 },
+	landmark: { type: String, max: 999 },
+	gmap: { type: String, max: 999 },
+	aliases: [String]
+});
+
+GymsSchema.index({ name: 1 }, { unique: true });
+
+module.exports = mongoose.model('Gym', GymsSchema);
